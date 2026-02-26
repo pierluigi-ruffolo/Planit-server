d5063b78455ee9209c6bad294af409b68b53b0db
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-02-26 15:23:18 +0100

    feat: add register and login controllers with bcrypt password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,64 @@
 import connection from "../db/connection.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+function register(req, res, next) {
+  const { name, surname, email, password } = req.body;
+  const saltRounds = 10;
+  const formatMail = email.toLowerCase();
+  const checkedMail = "select * from users where users.email = ?";
+  connection.query(checkedMail, [formatMail], (error, resalt) => {
+    if (error) return next(error);
+    if (resalt.length !== 0) {
+      return res.status(400).json({
+        Message:
+          "Questa email è già associata a un account. Prova a fare il login.",
+        error: "Error 400",
+      });
+    }
 
-function register(req, res) {
-  res.send("ciao da Register");
+    bcrypt.hash(password, saltRounds, function (error, hash) {
+      if (error) return next(error);
+      const insertUser =
+        "INSERT INTO users (name, surname, email, password) VALUES (?, ?, ?, ?)";
+      connection.query(
+        insertUser,
+        [name, surname, formatMail, hash],
+        (error, result) => {
+          if (error) return next(error);
+          res.status(201).json({
+            status: "success",
+            message: "Registrazione completata con successo!",
+            name: name,
+            surname: surname,
+            userId: result.insertId,
+          });
+        },
+      );
+    });
+  });
 }
 
-function login(req, res) {
-  res.send("ciao da Login");
+function login(req, res, next) {
+  const { email, password } = req.body;
+  const checkmail = "select * from users where email = ?";
+  connection.query(checkmail, [email], (error, result) => {
+    if (error) return next(error);
+    if (result.length === 0) {
+      return res.status(400).json({
+        Message: "Email non valida",
+        Error: "Error 400",
+      });
+    }
+    const keyPassword = result[0].password;
+    bcrypt.compare(password, keyPassword, function (err, result) {
+      if (!result) {
+        return res.status(400).json({
+          Message: "Password non valida",
+          Error: "Error 400",
+        });
+      }
+    });
+  });
 }
 
 function logout(req, res) {
