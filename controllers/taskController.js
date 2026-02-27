159dabf8c9748923d472b76a6864f93a7f51e60b
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-02-27 17:10:50 +0100

    add dynamic filters to task index and improve date validation

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,12 +1,36 @@
 import connection from "../db/connection.js";
-
 function index(req, res, next) {
-  let sql = `select users.id, users.name, users.surname, users.email, tasks.id as "tasks_id", tasks.title, tasks.description, tasks.status, tasks.priority, tasks.scheduled_at, categories.name as "categories_name", categories.color from users left join tasks on users.id = tasks.user_id left join categories on categories.id = tasks.category_id 	WHERE 1=1  and users.id = ?`;
+  const sqlUser =
+    "select users.id, users.name, users.surname, users.email from users where users.id = ?";
 
-  connection.query(sql, [10], (error, result) => {
+  let sqlTasks = `select tasks.id as "tasks_id", tasks.title, tasks.description, tasks.status,  tasks.priority, tasks.scheduled_at, categories.name as "categories_name",  categories.color  from tasks left join categories on categories.id = tasks.category_id WHERE tasks.user_id = ?`;
+  const { status, priority, category, sort } = req.query;
+  const params = [req.id];
+
+  if (status) {
+    sqlTasks += ` AND tasks.status = ?`;
+    params.push(status.toLowerCase());
+  }
+
+  if (priority) {
+    sqlTasks += " AND tasks.priority = ?";
+    params.push(priority.toLowerCase());
+  }
+
+  if (category) {
+    sqlTasks += " AND categories.name = ?";
+    params.push(category.toLowerCase());
+  }
+  let direction = "ASC";
+  if (sort === "furthest") {
+    direction = "DESC";
+  }
+  sqlTasks += ` ORDER BY tasks.scheduled_at IS NULL ASC, tasks.scheduled_at ${direction}`;
+
+  connection.query(sqlUser, [req.id], (error, result) => {
     if (error) return next(error);
     if (result.length === 0) {
-      return res.status(400).json({
+      return res.status(404).json({
         Message: "Utente non trovato",
         Error: "Error 400",
       });
@@ -16,23 +40,37 @@ function index(req, res, next) {
       name: result[0].name,
       email: result[0].email,
     };
+    connection.query(sqlTasks, params, (error, result) => {
+      if (error) return next(error);
+      let tasksUser = [];
+      if (result.length !== 0) {
+        const oggi = new Date();
+        oggi.setHours(0, 0, 0, 0);
+        tasksUser = result.map((t) => {
+          let isOverdue = false;
+          const taskDate = t.scheduled_at ? new Date(t.scheduled_at) : null;
+          if (taskDate < oggi && t.status !== "done" && taskDate !== null) {
+            isOverdue = true;
+          }
 
-    const tasksUser = result.map((t) => {
-      return {
-        task_id: t.tasks_id,
-        title: t.title,
-        description: t.description,
-        status: t.status,
-        priority: t.priority,
-        scheduled_at: t.scheduled_at,
-        categories: t.categories_name,
-        categories_color: t.color,
-      };
-    });
-
-    res.json({
-      user,
-      tasksUser: result[0].tasks_id === null ? [] : tasksUser,
+          return {
+            task_id: t.tasks_id,
+            title: t.title,
+            description: t.description,
+            status: t.status,
+            priority: t.priority,
+            scheduled_at: t.scheduled_at,
+            isOverdue,
+            categories: t.categories_name,
+            categories_color: t.color,
+          };
+        });
+      }
+      res.json({
+        user,
+        lengthTasks: tasksUser.length,
+        tasksUser,
+      });
     });
   });
 }
